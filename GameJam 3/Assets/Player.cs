915ee0a881be81ff2ca15495e95d2cc29fba8ef9
KurryW <kwesterhuis@hotmail.com>
2023-09-25 14:57:39 +0200

    level design and animations

--- GameJam 3/Assets/Player.cs
+++ GameJam 3/Assets/Player.cs
@@ -13,32 +13,32 @@ public class Player : MonoBehaviour {
 
     public float speed = 600.0f;
 	public float turnSpeed = 400.0f;
-	public float jumpHeight = 15f;
+	//public float jumpHeight = 15f;
 	
 	//jump
 	private float yVelocity;
 	private Vector3 moveDirection = Vector3.zero;
 	public float gravity = 20.0f;
-	[SerializeField]
-	bool isGrounded;
-	float beginJumpTime = 0;
+	//[SerializeField]
+	//bool isGrounded;
+	//float beginJumpTime = 0;
+	
+ //   private void OnTriggerEnter(Collider other)
+	//{
 
-    private void OnTriggerEnter(Collider other)
-	{
+	//	if (other.gameObject.tag== "Ground")
+	//	{
+	//		isGrounded = true;
+	//	}
+	//}
+ //   private void OnTriggerExit(Collider other)
+	//{
 
-		if (other.gameObject.tag== "Ground")
-		{
-			isGrounded = true;
-		}
-	}
-    private void OnTriggerExit(Collider other)
-	{
-
-		if (other.gameObject.tag == "Ground")
-		{
-			isGrounded = false;
-		}
-	}
+	//	if (other.gameObject.tag == "Ground")
+	//	{
+	//		isGrounded = false;
+	//	}
+	//}
 
 	void Start () 
 	{
@@ -52,43 +52,53 @@ public class Player : MonoBehaviour {
 		Vector3 direction = new Vector3(horizontalInput, 0, 0);
 		Vector3 velocity = direction * speed;
 
-		if (Input.GetKey(KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.W))
 		{
-			anim.SetInteger("AnimationPar", 1);
-			footstepsSound.enabled = true;
+			anim.SetInteger("Walk", 1);
+			//footstepsSound.enabled = true;
 		}
-		else
+		else if (Input.GetKeyUp(KeyCode.W))
 		{
-			anim.SetInteger("AnimationPar", 0);
-			footstepsSound.enabled = false;
+			anim.SetInteger("Walk", 0);
+			//footstepsSound.enabled = false;
 		}
 
-
-		moveDirection = transform.forward * Input.GetAxis("Vertical")  * speed;
-
-		if (isGrounded)
-		{
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				yVelocity = jumpHeight;
-				beginJumpTime = Time.time;
-				anim.SetInteger("JumpPar", 1);
-			}
-		}
-        else
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            yVelocity -= gravity;
-            anim.SetInteger("JumpPar", 0);
+            anim.SetInteger("punch", 1);
+            //footstepsSound.enabled = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetInteger("punch", 0);
+            //footstepsSound.enabled = false;
         }
 
+        moveDirection = transform.forward * Input.GetAxis("Vertical")  * speed;
+
+		//if (isGrounded)
+		//{
+		//	if (Input.GetKeyDown(KeyCode.Space))
+		//	{
+		//		yVelocity = jumpHeight;
+		//		beginJumpTime = Time.time;
+		//		anim.SetInteger("JumpPar", 1);
+		//	}
+		//}
+  //      else
+  //      {
+  //          yVelocity -= gravity;
+  //          anim.SetInteger("JumpPar", 0);
+  //      }
 
 
-        float currentAirTime = Time.time - beginJumpTime;
 
-		if (controller.isGrounded == true && (currentAirTime >= 90f && currentAirTime <= 139f))
-		{
-            SceneManager.LoadScene("GameOver");
-        }
+        //float currentAirTime = Time.time - beginJumpTime;
+
+		//if (controller.isGrounded == true && (currentAirTime >= 90f && currentAirTime <= 139f))
+		//{
+  //          SceneManager.LoadScene("GameOver");
+  //      }
 
 
 		
